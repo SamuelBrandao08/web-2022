9ae47b807da1801762e1b3c0ce898bf3fbff4bfc
samuelbrandao <samuelbrandao08@gmail.com>
2022-06-09 20:58:35 -0300

    Atividade de web: backend construido com express e mongoose conectando-se com mongoDB.

---
+++ back-end/src/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+//var path = require("path");
+//var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+require("./db/mongo.connection");
+
+//var students = require("./routes/student/StudentRoute");
+//var professors = require("./routes/professor/ProfessorRoute");
+
+var students = require("./routes/student/students.routes.mongo");
+var professors = require("./routes/professor/professors.routes.mongo");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+app.use("/students", students);
+app.use("/professors", professors);
+
+module.exports = app;
+
+app.listen(3001, function () {
+  console.log("server running on 3001");
+}); //the server object listens on port 8080
